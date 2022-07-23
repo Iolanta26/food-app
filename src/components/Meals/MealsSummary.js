e09699811f3055bd3f46c002dfaf3de5bf36ce3c
iolantascheifel <iolanta.scheifel@gmail.com>
2022-07-23 18:13:58 +0200

    style updated

--- src/components/Meals/MealsSummary.js
+++ src/components/Meals/MealsSummary.js
@@ -8,9 +8,10 @@ const MealsSummary = () => {
         Choose your favorite meal from our broad selection of available meals
         and enjoy a delicious lunch or dinner at home.
       </p>
-      <p>
-        All our meals are cooked with high-quality ingredients, just-in-time and
-        of course by experienced chefs!
+      <p className={classes.myText}>
+        Project is made by Iolanta Scheifel. Technologies used: React, Context
+        API, Backend is linked to Firebase. Submitted order's info goes to
+        Firebase database.
       </p>
     </section>
   );
